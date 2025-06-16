390cfbb2ead3e5522fba5e224a58b1740d07651a
K.K <Kakumori.Koki@trans-cosmos.co.jp>
2025-06-16 16:24:49 +0900

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+
+    public function index() {
+
+        $users = User::orderby('id','desc')->paginate(10);
+
+        return view('users.index',[
+            'users' => $users,
+        ]);
+    }
+
+    public function show(string $id) {
+
+        $user = User::findOrfail($id);
+
+        return view('users.show',[
+            'user' => $user,
+        ]);
+    }
+
+}
